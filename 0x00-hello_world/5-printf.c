16a8cd594d3818e139cb664af960c9fd670b178f
theurikenneth <theurikenneth@hmail.com>
2021-02-03 13:30:21 +0000

    C-Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
-#!/bin/bash
-
 #include <stdio.h>
 
-int main (void)
+/**
+* main - ouptputs the message
+* Description : uses printf
+* Return : void
+*
+*/
+int main(void)
 {
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-
-    return (0);
+return (0);
 }
