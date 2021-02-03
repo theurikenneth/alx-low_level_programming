2a55c8b962a750c2cc3a8a0b719e95773511d920
theurikenneth <theurikenneth@hmail.com>
2021-02-03 13:18:32 +0000

    C-Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-/*
+/**
  * main - this is the entry point
+ *
  * return - it is always 0 for success
-*/
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
