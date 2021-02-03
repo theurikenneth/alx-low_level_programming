76390028af92870155859fcfc8a23f8738b2d679
theurikenneth <theurikenneth@hmail.com>
2021-02-03 06:41:18 +0000

    C-Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    puts("\"Programming is like building a multilingual puzzle");
+
+    return (0);
+}
+
+
+/*
+* main - all C programs must start with main with data type int.
+*Return - it must be 0
+*/
