6ab274813c0a18d1e681153a73adfbc652c2ea10
theurikenneth <theurikenneth@hmail.com>
2021-02-03 14:00:02 +0000

    C-Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
 #include <unistd.h>
 
+/**
+ * main - writes text
+ * description - uses write to stdout
+ * Return: returns 1
+ */
 int main(void)
 
 {
-  write ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-  return (1);
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
