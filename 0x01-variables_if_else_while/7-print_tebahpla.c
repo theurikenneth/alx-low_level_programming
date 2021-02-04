0d4833b753f80aa0651915db24fab0367c6b541f
theurikenneth <theurikenneth@hmail.com>
2021-02-04 15:50:28 +0000

    Variables

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char rev
+
+for (rev = 'z'; rev >= 'a'; rev--)
+putchar(rev);
+
+puchar('\n')
+
+return (0);
+}
