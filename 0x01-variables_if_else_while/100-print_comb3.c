653e9c0a8e53974e0f780ea3ccef4c44098adc4f
theurikenneth <theurikenneth@hmail.com>
2021-02-04 18:19:53 +0000

    Variables

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int m, n;
+
+for (m = 0; m < 9; m++)
+{
+for (n = m + 1; n < 10; n++)
+{
+putchar((m % 10) + '0');
+putchar((n % 10) + '0');
+
+if (m == 8 && n == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+
+}     
