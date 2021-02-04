042371b747328a126b4b31f6106de502b615b427
theurikenneth <theurikenneth@hmail.com>
2021-02-04 08:18:59 +0000

    Variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letters;
+
+for (letters = 'a'; letters <= 'z'; letters++)
+putchar(letters);
+putchar('\n');
+return (0);
+}
