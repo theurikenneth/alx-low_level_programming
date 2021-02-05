ec889e9945524b7e65936faa470bf982acc6a660
theurikenneth <theurikenneth@hmail.com>
2021-02-05 09:48:44 +0000

    C-Hello, World

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int m, n, o;
+
+for (m = 0; m <= 7; m++)
+{
+for (n = m + 1; n <= 8; n++)
+{
+for (o = n + 1; o <= 9; o++)
+{
+putchar((m % 10) + '0');
+putchar((n % 10) + '0');
+putchar((o % 10) + '0');
+
+if (m == 7 && n == 8 && o == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
