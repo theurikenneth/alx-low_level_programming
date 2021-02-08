dd0a1b997d89119abd8ba1c70126dacdb053274a
theurikenneth <theurikenneth@hmail.com>
2021-02-08 14:45:45 +0000

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * times_table - write a function that prints the 9 times table, starting with 0
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+
+}
