ba6f1b86f7a18265d6d898ff6d0f26c63c71b11b
theurikenneth <theurikenneth@hmail.com>
2021-02-08 09:56:29 +0000

    Functions, nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * _islower - check if c is lowercase 
- *
+ * _islower - check if "c" is lowercase
+ * @c - it will be checked
  * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer(void) - prints every minute of the day of Jack Bauer
+ *
+ * Return: always 0 (Success)
+ */
+void jack_bauer(void)
+{
+
+}
