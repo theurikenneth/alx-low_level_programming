054fca983955894a56386e71efb2947d63b3e22f
theurikenneth <theurikenneth@hmail.com>
2021-02-08 20:49:35 +0000

    Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,5 +7,22 @@
  */
 void times_table(void)
 {
+int n, m, mult;
+for (n = 0; n <= 9, n++)
+{
+_putchar('0');
+for (m = 1; m <= 9; m++)
+{
+_putchar(',');
+_putchar(' ');
+mult = n * m;
 
+if (mult <= 9)
+_putchar(' ');
+else
+_putchar((mult / 10) + '0');
+_putchar((mult % 10) + '0');
+}
+_putchar('\n');
+}
 }
