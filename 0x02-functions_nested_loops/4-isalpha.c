de328a6fe8b0e335fb3ae0f45e7dd232a863ec96
theurikenneth <theurikenneth@hmail.com>
2021-02-09 06:40:19 +0000

    Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,11 @@
 
 /**
  * _isalpha - tests whether c is a letter, lowercase or uppercase
+ * @c: the character to be tested
  *
  * Return: 1 if c is a letter, lowercase or uppercase and 0 otherwise
  */
 int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-return (1);
-else
-return (0);
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
