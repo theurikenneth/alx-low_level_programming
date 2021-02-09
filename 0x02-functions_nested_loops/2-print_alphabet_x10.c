3bc94757ceff3297dbd7f049dfea01cdc7edcb3d
theurikenneth <theurikenneth@hmail.com>
2021-02-09 07:20:24 +0000

    Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,21 +7,21 @@
  */
 void print_alphabet_x10(void)
 {
-int n;
-for (n = 0; n < 10; n++)
-print_alphabet();
+int a, n;
+
+n = 0;
+
+while (n < 10)
+{
+a = 'a';
+while (a <= 'z')
+{
+_putchar(a++);
+}
+_putchar(10);
+n++;
 }
 
-/**
- * print_alphabet - prints the alphabet
- *
- * Return: Always 0
- */
+return;
 
-void print_alphabet(void)
-{
-char a;
-for (a = 'a'; a <= 'z'; a++)
-_putchar(c);
-_putchar('\n');
 }
