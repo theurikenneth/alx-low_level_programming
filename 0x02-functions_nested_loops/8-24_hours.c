0f4fc9e89a8bcef8e31e3ad1bf2feed04fecf0a2
theurikenneth <theurikenneth@hmail.com>
2021-02-09 06:54:00 +0000

    Functions, nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,14 +3,14 @@
 /**
  * jack_bauer(void) - prints every minute of the day of Jack Bauer
  *
- * Return: always 0 (Success)
+ * Return: void
  */
 void jack_bauer(void)
 {
 int hour, minute;
-for (hour = 0; hour <= 23; hour++)
+for (hour = 0; hour < 24; hour++)
 {
-for (minute = 0; minute <= 59; minute++)
+for (minute = 0; minute < 60; minute++)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
