0178c63d8f5714080c49d9c013d958cb0a668674
theurikenneth <theurikenneth@hmail.com>
2021-02-09 10:13:05 +0000

    c-debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include <stdio.h>
 
 /**
  * main - causes an infinite loop
@@ -7,11 +7,15 @@
 
 int main(void)
 {
-int i;
+/*
+ *int i;
+ */
 
 printf("Infinite loop incoming :(\n");
 
-i = 0;
+/*
+ *i = 0;
+ */
 
 /*
  * while (i < 10)
