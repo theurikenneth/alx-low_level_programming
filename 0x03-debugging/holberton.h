faf75a8b14ba7e5c301c9c250be7617c3738c07a
theurikenneth <theurikenneth@hmail.com>
2021-02-09 10:08:05 +0000

    c-debugging

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -3,7 +3,14 @@
 
 #include <stdio.h>
 
-int main(void);
 void positive_or_negative(int i);
+
+int main(void);
+
+int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
+
+void print_remianing_days(int month, int day, int year);
 
 #endif /* HOLBERTON_H */
