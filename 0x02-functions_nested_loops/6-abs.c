01099cf2a4a693bf563e157f089147bb4db3fc49
theurikenneth <theurikenneth@hmail.com>
2021-02-09 06:45:35 +0000

    Functions, nested loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * _abs - computes the absolute value of int
+ * @n: the integer to value absolutely
  *
  * Return: returns n if positive and -n if negative
  */
