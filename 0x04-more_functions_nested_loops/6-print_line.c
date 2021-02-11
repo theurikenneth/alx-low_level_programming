55c76e88f939cf426ed15307e7a46481af4a4978
theurikenneth <theurikenneth@hmail.com>
2021-02-11 11:25:45 +0000

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_line - prints character _
+ *@n: number of times to print character _
+ * Return: void
+ */
+void print_line(int n)
+{
+int a;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (a = 0; a < n; a++)
+{
+_putchar(1);
+}
+_putchar('\n');
+}
+}
