c9a8b98cd758dd5c0275efae00a4c745236b299a
theurikenneth <theurikenneth@hmail.com>
2021-02-11 12:27:31 +0000

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,13 +15,13 @@ if ((a % 3 == 0) && (a % 5 == 0))
 {
 printf("FizzBuzz");
 }
-else if (a % 5 == 0)
-{
-printf("Buzz");
-}
 else if (a % 3 == 0)
 {
 printf("Fizz");
+}
+else if (a % 5 == 0)
+{
+printf("Buzz");
 }
 else
 {
