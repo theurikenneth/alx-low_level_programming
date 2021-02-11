4406dd6ba36ca1b46a5ae3909bdca7bbb777fb9c
theurikenneth <theurikenneth@hmail.com>
2021-02-11 11:42:21 +0000

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_line - prints character _
+ *@n: number of times to print character _
+ * Return: void
+ */
+void print_line(int n)
+{
+int a, b;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (a = 0; a < n; a++)
+{
+for (b = 0; b < a; b++)
+{
+_putchar(32);
+}
+_putchar(92);
+_putchar('\n');
+}
+}
+}
