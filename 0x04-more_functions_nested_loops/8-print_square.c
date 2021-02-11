05e6fbac2a30003fc728c709c4cda6d4cd2d26a0
theurikenneth <theurikenneth@hmail.com>
2021-02-11 11:56:19 +0000

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_square - prints character _
+ *@size: number of times to print character _
+ * Return: void
+ */
+void print_square(int size)
+{
+int a, b;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < a; b++
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
