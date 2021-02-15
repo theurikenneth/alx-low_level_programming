c3c6ad478105b6cce006a8805d6bc874ef6e3a1d
theurikenneth <theurikenneth@hmail.com>
2021-02-15 22:08:40 +0000

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,14 +8,11 @@
  */
 void puts2(char *str)
 {
-int i, j = 0;
-for (; str[j] != '\0')
-{
-j++;
-}
+int i, j;
+for (j = 0; str[j] != '\0'; j++)
+;
 for (i = 0; i < j; i += 2)
-{
 _putchar(str[i]);
-}
+
 _putchar('\n');
 }
