af35ee13701228008b0a7b110326d0a057218a33
theurikenneth <theurikenneth@hmail.com>
2021-02-15 22:27:59 +0000

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int i;
-for (i = 0; src[i] != '\0'; i++
+int i = 0;
+if (dest == src || src == 0)
+return (dest);
+while (src[i])
 {
 dest[i] = src[i];
+i++;
 }
-dest[i] = '\0';
+dest[i] = src[i];
 return (dest);
 }
