ee9859fd45f48c848935fb4335a9955305bfafd3
theurikenneth <theurikenneth@gmail.com>
2021-02-18 08:45:10 +0000

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concetenates two strings
+ * @dest: string is appended to dest
+ * @src: string to be appended
+ * Return: void
+ */
+char *_strcat(char *dest, char *src);
+{
+int a, b;
+for (a = 0; dest[a] != '\0'; a++)
+{
+
+}
+for (j = 0; src[b] != '\0'; b++)
+{
+dest[a] = src[b];
+a++;
+}
+return (dest);
+}
