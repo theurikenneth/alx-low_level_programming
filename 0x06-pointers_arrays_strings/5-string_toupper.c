1e4eabc3cee1348ecae4f777c63af6eff0f8e0bf
theurikenneth <theurikenneth@gmail.com>
2021-02-18 10:51:02 +0000

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,7 @@
 /**
  * string_toupper - converts the string to upper case
  * @s1: string to convert
- * Return: void
+ * Return: char pointer
  */
 char *string_toupper(char *s1)
 {
@@ -13,5 +13,5 @@ for (i = 0; s1[i] != '\0'; i++)
 if ((s1[i] >= 'a') && (s1[i] <= 'z'))
 s1[i] = s1[i] - 32;
 }
-return(s1);
+return (s1);
 }
