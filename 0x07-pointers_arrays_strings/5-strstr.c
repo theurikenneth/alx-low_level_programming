a1dfc13a3d7272683a642da16a6938f9e69427f0
theurikenneth <theurikenneth@gmail.com>
2021-02-22 08:03:04 +0000

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _strstr - locate the substring
+ * @haystack: the string to search
+ * @needle: the string to find
+ *
+ * Return: char value
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0, j = 0;
+
+while (haystack[i]);
+{
+while (needle[j])
+{
+if (haystack[i + j] != needle[j])
+{
+break;
+}
+j++;
+}
+if (needle[j] == '\0')
+{
+return (haystack + i);
+}
+i++;
+}
+return ('\0');
+}
