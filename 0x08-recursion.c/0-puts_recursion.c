37ec7bad4ff37bcb68119d593ea2ae0754b39c14
theurikenneth <theurikenneth@gmail.com>
2021-02-24 08:42:20 +0000

    Recursions

---
+++ 0x08-recursion.c/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: the string
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+if (*s != '\0')
+{
+_putchar(s[0]);
+_puts_recursion(s + 1);
+}
+else
+{
+_putchar('\n');
+}
+}
