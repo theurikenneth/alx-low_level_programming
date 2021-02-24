ea7cbb94e628f8383f40560cf312f9dbb3c2fda2
theurikenneth <theurikenneth@gmail.com>
2021-02-24 10:51:02 +0000

    Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,10 +1,13 @@
 #include "holberton.h"
+#include <stdio.h>
+
+int check_prime(int n, int i);
 
 /**
  * is_prime_number - checks whether integer is a prime number
  * @n: the number to be checked
  *
- * Return: whether or not its a prime number 
+ * Return: whether or not its a prime number
  */
 int is_prime_number(int n)
 {
@@ -14,17 +17,20 @@ return (check_prime(n, 1));
 /**
  * check_prime - checks whether a number is prime
  * @n: the number
- * @i: the iteration times 
+ * @i: the iteration times
  *
  * Return: 1 for prime, 0 for composite
  */
 int check_prime(int n, int i)
 {
-for (i = 2; i <= n / 2; i++)
-
-if (n % i == 0)
-{
-return (1);
-}
+if (n <= 1)
 return (0);
+
+if (n % i == 0 && i > 1)
+return (0);
+
+if ((n / i) < i)
+return (1);
+
+return (check_prime(n, i + 1));
 }
