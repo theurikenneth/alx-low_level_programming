cc045091ee9610694ac3e809994eec883e500dd6
theurikenneth <theurikenneth@gmail.com>
2021-02-24 10:36:42 +0000

    Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,15 +8,23 @@
  */
 int is_prime_number(int n)
 {
-int i;
+return (check_prime(n, 1));
+}
 
-for (i = 2; i <= n / 2; i++)
+/**
+ * check_prime - checks whether a number is prime
+ * @n: the number
+ * @i: the iteration times 
+ *
+ * Return: 1 for prime, 0 for composite
+ */
+int check_prime(int n, int i)
 {
+for (i = 2; i <= n / 2; i++)
+
 if (n % i == 0)
 {
 return (1);
 }
-}
-
 return (0);
 }
