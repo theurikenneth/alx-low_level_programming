f5f564814fe93f26ee5804d324afdf8f25cd0c8e
theurikenneth <theurikenneth@gmail.com>
2021-03-02 17:59:15 +0000

    C-malloc, free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,18 +4,13 @@
 
 /**
  * free_grid - frees a 2D array of integers
- * @grid: 2D to free
+ * @grid: rows of matrix
  * @height: height of the grid
  * Return: Nothing
  */
 void free_grid(int **grid, int height)
 {
-  int i, *n;
-
-if (grid == NULL || grid == 0)
-{
-return;
-}
+int i, *n;
 
 for (i = 0; i < height; i++)
 {
