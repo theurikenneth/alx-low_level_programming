7f8547ddfe7bbd141a15d54a584a35d30a149dd5
theurikenneth <theurikenneth@gmail.com>
2021-03-03 06:57:53 +0000

    C-malloc, free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,18 +17,18 @@ int i, j;
 if (width <= 0 || height <= 0)
 return (NULL);
 
-twoD = malloc(sizeof *twoD * height);
+twoD = (int **)malloc(sizeof(int *) * height);
 
 if (twoD == NULL)
 return (NULL);
 
 for (i = 0; i < height; i++)
 {
-twoD[i] = malloc(sizeof i * width);
+twoD[i] = (int *)malloc(sizeof(int) * width);
 
 if (twoD[i]  == NULL)
 {
-free(twoD, i);
+free(twoD);
 return (NULL);
 }
 for (j = 0; j < width; j++)
