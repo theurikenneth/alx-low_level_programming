3bfdc8d0b752e30f127bcf5d5ca2acf2797fd1e5
theurikenneth <theurikenneth@gmail.com>
2021-03-04 11:46:22 +0000

    C-More malloc, free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *concat;
-unsigned int length = n, i;
+int n1 = 0, n2 = 0;
 
 if (s1 == NULL)
 s1 = "";
@@ -20,23 +20,23 @@ s1 = "";
 if (s2 == NULL)
 s2 = "";
 
-for (i = 0; s1[i]; i++)
-length++;
+for (n1 = 0; s1[n1] != '\0'; n1++)
+;
+for (n2 = 0; s2[n2] != '\0'; n2++)
+;
 
-concat = malloc(sizeof(char) * length + 1));
+concat = malloc(sizeof(char) * n1 + n2 + 1));
 
 if (concat == NULL)
 return (NULL);
 
-length = 0;
+for (n = 0, s1[n] != '\0'; n++)
+*concat = s1[n];
 
-for (i = 0, s1[i]; i++)
-concat[length++] = s1[i];
+for (n = 0; s2[n2] != '\0'; n++)
+*concat = s2[n];
 
-for (i = 0; s2[i] && i < n; i++)
-concat[length++] = s2[i];
+*concat = '\0';
 
-concat[length++] = '\0';
-
-return (concat);
+return (concat - n1 - n2);
 }
