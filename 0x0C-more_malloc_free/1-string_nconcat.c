44384231e17d3c0e865a6ca107e36d6315bdebc9
theurikenneth <theurikenneth@gmail.com>
2021-03-04 11:56:54 +0000

    C-More malloc, free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,12 +14,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *concat;
 int n1 = 0, n2 = 0;
 
-if (s1 == NULL)
-s1 = "";
-
-if (s2 == NULL)
-s2 = "";
-
 if (s1)
 {
 while (s1[n1])
