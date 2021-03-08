e38d0781e827b5205dea32ec98aa63a5ed3928a1
theurikenneth <theurikenneth@gmail.com>
2021-03-08 09:45:45 +0000

    C-Structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,67 +11,76 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *d;
+int new_name = 0, new_owner = 0;
 
 if (name != NULL && owner != NULL)
 {
+new_name = _strlen(name) + 1;
+new_owner = _strlen(owner) + 1;
 d = malloc(sizeof(dog_t));
 
 if (d == NULL)
 return (NULL);
 
-if (name)
-{
-d->name = _previous(name);
-if (!(d->name))
+d->name = malloc(sizeof(char) * new_name);
+
+if (d->name == NULL)
 {
 free(d);
 return (NULL);
 }
-}
-else
-d->name - NULL;
 
-d->age = age;
+d->owner = malloc(sizeof(char) * new_owner);
 
-if (owner)
-{
-d->owner = _previous(owner);
-if (!(d->owner))
+if (d->owner == NULL)
 {
 free(d->name);
 free(d);
 return (NULL);
 }
-}
-else
-d->owner = NULL;
+
+d->name = _strcpy(d->name, name);
+d->owner = _strcpy(d->owner, owner);
+d->age = age;
 }
 return (d);
 }
 
 /**
- * _previous - creates an array of a copy of the string
- * @str: a pointer to the copied string
- * Return: void
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
  */
-char *_previous(char *str)
+int _strlen(char *s)
 {
-char *copy;
-unsigned int i = 0;
+int c = 0;
 
-if (str)
+for (; *s != '\0'; s++)
 {
-while (str[i++])
-;
-copy = malloc(sizeof(char) * i);
-
-if (copy)
-{
-while (i--)
-copy[i] = str[i];
-
-return (copy);
+c++;
 }
+
+return (c);
 }
-return (NULL);
+
+/**
+ * _strcpy - Copy a string
+ * @dest: Destination value
+ * @src: Source value
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+
+dest[i++] = '\0';
+
+return (dest);
 }
