be94703aef06ae16afde40f82be84443e42840dc
theurikenneth <theurikenneth@gmail.com>
2021-03-10 14:46:19 +0000

    C-Function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,20 +12,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 
-if (cmp != NULL && array != NULL)
+if (cmp && array)
 {
-if (size <= 0)
-return (-1);
-
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]))
-break;
-
-if !cmp(array[size - 1]))
-return (-1);
-}
 return (i);
+}
 }
 return (-1);
 }
