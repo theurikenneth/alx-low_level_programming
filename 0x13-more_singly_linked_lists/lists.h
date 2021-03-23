382053630404014756ae7a1f7b7c8f7e05ef7009
theurikenneth <theurikenneth@gmail.com>
2021-03-23 06:51:27 +0000

    More singly linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,7 +18,8 @@ typedef struct listint_s
   struct listint_s *next;
 } listint_t;
 
-size_t printlistint(const listint_t *h);
+int _putchar(char c);
+size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
