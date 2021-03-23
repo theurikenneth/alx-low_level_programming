9a34797cb496a40ca8fb8ef3c7a555ea03390196
theurikenneth <theurikenneth@gmail.com>
2021-03-23 06:17:08 +0000

    More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,17 +9,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-int len = 0;
 
 if (h != NULL)
 {
-while (h)
-{
 printf("%d\n", h->n);
-h = h->next;
-len++;
-}
+return (print_listint(h->next) + 1);
 }
 
-return (len);
+return (0);
 }
