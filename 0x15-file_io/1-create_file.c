de28194361c710c3d328c1250d397b7324dccc48
theurikenneth <theurikenneth@gmail.com>
2021-03-30 09:18:42 +0000

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,24 +9,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int o, w, len = 0;
+int fd;
+ssize_t d, i = 0;
 
-if (filename == NULL)
+if (!filename)
 return (-1);
 
-if (text_content != NULL)
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
+
+if (text_content)
 {
-for (len = 0; text_content[len];)
-len++;
+while (text_content[i])
+i++;
+d = write(fd, text_content, i);
+if (d != i)
+return (-1);
 }
 
-o = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
-w = write(o, text_content, len);
-
-if (o == -1 || w == -1)
-return (-1);
-
-close(o);
+close(fd);
 
 return (1);
 }
