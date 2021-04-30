95fcab2644c4998dbd54ccf593f2a9733866072b
theurikenneth <theurikenneth@gmail.com>
2021-04-30 10:50:02 +0000

    C- Doubly linked lists

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - the number of elements
+ * @h: head of the double list
+ *
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+int i = 0;
+
+while (h != NULL)
+{
+i++;
+h = h->next;
+}
+return (i);
+}
