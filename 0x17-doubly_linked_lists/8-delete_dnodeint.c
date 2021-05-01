a32a0bbfe5fba94de229f71890ce6c4cd863d1dd
theurikenneth <theurikenneth@gmail.com>
2021-05-01 18:15:35 +0000

    C - Doubly linked lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,101 +11,43 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-dlistint_t *current = NULL, *temp = NULL;
-unsigned int length = 0;
+dlistint_t *old, *prev;
 
-if (head && *head)
-{
-length = dlistint_len(*head);
-if (index > length)
+if (!head)
 return (-1);
 
-if (index == 0)
-return (delete_first_dnode(head));
+old = *head;
 
-current = get_dnodeint_at_index(*head, index);
-if (current)
-{
-temp = current;
-if (length - 1 == index)
-current->prev->next = current->next;
-else
-{
-current->prev->next = current->next;
-current->next->prev = current->prev;
-}
-free(temp);
-return (1);
-}
-}
+if (!old)
 return (-1);
-}
 
-/**
- * delete_first_dnode - removes the first node of a doubly linked list
- * @head: the head of the doubly linked list
- *
- * Return: 1 if is deleted
- */
-int delete_first_dnode(dlistint_t **head)
+if (index)
 {
-dlistint_t *current = *head, *temp = NULL;
+prev = old;
 
-temp = current;
-if (current->next)
-{
-current = current->next;
-current->prev = temp->prev;
-*head = current;
+while (--index && prev->next)
+prev = prev->next;
+
+if (index)
+return (-1);
+
+old = prev->next;
+
+if (!old)
+return (-1);
+
+prev->next = old->next;
+
+if (prev->next)
+prev->next->prev = prev;
 }
 else
 {
-*head = NULL;
+*head = old->next;
+
+if (*head)
+(*head)->prev = NULL;
 }
-free(temp);
+free(old);
 return (1);
-}
-
-/**
- * get_dnodeint_at_index - gets a node from a doubly linked list
- * @head: the head of the doubly linked list
- * @index: the index to find in the doubly linked list
- *
- * Return: the specific node of the doubly linked list
- */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
-{
-dlistint_t *current = head;
-unsigned int iter_times = 0;
-
-if (head)
-{
-while (current != NULL)
-{
-if (iter_times == index)
-return (current);
-
-current = current->next;
-++iter_times;
-}
-}
-return (NULL);
-}
-
-/**
- * dlistint_len - counts the number of elements in a doubly linked list
- * @h: the double linked list to count
- *
- * Return: no. of elements in the doubly linked list
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-int length = 0;
-
-while (h != NULL)
-{
-++length;
-h = h->next;
-}
-return (length);
 }
