36afd875f9b161777417ff40c6d9e3c1d4a901aa
theurikenneth <theurikenneth@gmail.com>
2021-05-01 17:07:05 +0000

    C - Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,18 +8,14 @@
  *
  * Return: nodes
  */
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
-unsigned int i = 0;
+size_t i = 0;
 
-if (h == NULL)
-return (0);
-
-while (h != NULL)
+for ( ; h; h = h->next)
 {
 printf("%d\n", h->n);
-i++;
-h = h->next;
+++i;
 }
 return (i);
 }
