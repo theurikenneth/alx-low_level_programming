59e62aa94d1f508366ad94cd653f4b763f76aea1
theurikenneth <theurikenneth@gmail.com>
2021-05-01 17:00:33 +0000

    C - Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,12 +10,15 @@
  */
 size_t print_dlistint(const dlistint_t *h);
 {
-int i = 0;
+unsigned int i = 0;
+
+if (h == NULL)
+return (0);
 
 while (h != NULL)
 {
 printf("%d\n", h->n);
-++i;
+i++;
 h = h->next;
 }
 return (i);
