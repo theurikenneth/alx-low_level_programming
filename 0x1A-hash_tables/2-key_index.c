e8d1a3da1c0234daa1f9084ecb9e4adcbcf41009
theurikenneth <theurikenneth@gmail.com>
2021-05-27 19:24:56 +0000

    C - Hash tables

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - returns key index using djb2 hash value
+ * @key: key string
+ *
+ * Return: the key index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+return (hash_djb2(key) % size);
+}
