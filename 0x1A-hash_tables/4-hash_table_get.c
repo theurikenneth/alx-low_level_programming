6faf1f264c65e38bd0e0d927884618cb4596f25c
theurikenneth <theurikenneth@gmail.com>
2021-05-28 10:42:49 +0000

    C - Hash tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,21 +13,21 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-  hash_node_t *ptr;
-  unsigned long int index;
+hash_node_t *ptr;
+unsigned long int index;
 
-  if (ht == NULL || ht->size == 0 ||
-      key == NULL || *key == 0)
-    return (NULL);
-  index = hash_djb2((const unsigned char *) key);
-  index %= ht->size;
-  ptr = ht->array[index];
-  while (ptr != NULL)
-    {
-      if (strcmp(ptr->key, key) == 0)
-	break;
-    }
-  if (ptr == NULL)
-    return (NULL);
-  return (strdup(ptr->value));
+if (ht == NULL || ht->size == 0 ||
+key == NULL || *key == 0)
+return (NULL);
+index = hash_djb2((const unsigned char *) key);
+index %= ht->size;
+ptr = ht->array[index];
+while (ptr != NULL)
+{
+if (strcmp(ptr->key, key) == 0)
+break;
+}
+if (ptr == NULL)
+return (NULL);
+return (strdup(ptr->value));
 }
