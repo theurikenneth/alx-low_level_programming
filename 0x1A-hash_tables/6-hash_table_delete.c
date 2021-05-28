d375221f615182df602d4914bff3140c98eea33d
theurikenneth <theurikenneth@gmail.com>
2021-05-28 10:51:32 +0000

    C - Hash tables

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,25 +10,19 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-hash_node_t *next;
-unsigned long int i;
+  hash_node_t *ptr, *next;
+  unsigned long int idx;
 
-if (ht == NULL || ht->array == NULL || ht->size == 0)
-return;
-
-for (i = 0; i < ht->size; i++)
-{
-while (ht->array[i] != NULL)
-{
-next = ht->array[i]->next;
-free(ht->array[i]->key);
-free(ht->array[i]->value);
-free(ht->array[i]);
-ht->array[i] = next;
-}
-}
-free(ht->array);
-ht->array = NULL;
-ht->size = 0;
-free(ht);
+  if (ht == NULL)
+    return;
+  for (idx = 0; idx < ht->size; idx++)
+    for (ptr = ht->array[idx]; ptr != NULL; ptr = next)
+      {
+	next = ptr->next;
+	free(ptr->key);
+	free(ptr->value);
+	free(ptr);
+      }
+  free(ht->array);
+  free(ht);
 }
