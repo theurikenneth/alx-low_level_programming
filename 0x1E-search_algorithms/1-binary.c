0994699ba6d54e945338590033162d7a92da14b5
theurikenneth <theurikenneth@gmail.com>
2021-09-04 09:55:57 +0000

    C - Search Algorithms

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,51 +1,45 @@
 #include "search_algos.h"
+
 /**
- * binary_search - searches for a value in a sorted array of integers
- * @array: pointer to the first element of the array to search in
- * @size: number of elements in array
- * @value: value to search for
- * Return: index of value or -1
+ * print_array - print
+ * @array: The sort
+ * @left: min
+ * @right: max
+ * Return: Not
+ */
+void print_array(int *array, int left, int right)
+{
+int i;
+
+for (i = left; i < right; i++)
+printf("%d, ", array[i]);
+printf("%d\n", array[i]);
+}
+
+/**
+ * binary_search - seacrh dividin
+ * @array: the sort
+ * @size: size of
+ * @value: value to
+ * Return: The index of the value
  */
 int binary_search(int *array, size_t size, int value)
 {
-/* making declarations */
-int low, mid, high;
+int left = 0;
+int right = (int)size - 1;
+int i = 0;
 
-low = 0;
-high = (int)size - 1;
-
-/* check for nulls in the array */
-if (!array)
-return (-1);
-
-while (low <= high)
-{
-print_it(array, low, high)
-mid = (low + high) / 2;
-if (array[mid] < value)
-low = mid + 1;
-else if (array[mid] > value)
-high = mid - 1;
-else
-return (mid);
-}
-return (-1);
-}
-
-/**
- * print_it - prints the array searched
- * @arr: first element of the array
- * @first: [0] first element
- * @last: last element to print
- */
-void print_it(int *arr, int first, int last)
+while (left <= right)
 {
 printf("Searching in array: ");
-for (; first <= last; first++)
-{
-if (first == last)
-printf("%d\n", arr[first]);
+print_array(array, left, right);
+i = (left + right) / 2;
+if (array[i] < value)
+left = i + 1;
+else if (array[i] > value)
+right = i - 1;
 else
-printf("%d, ", arr[first]);
+return (i);
 }
+return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,17 +1,17 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
+#ifndef SORT_ALGO
+#define SORT_ALGO
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
 /**
- * struct listint_s - singly linked list
+ * struct listint_s - singly
  *
  * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
+ * @index: Index of
+ * @next: Pointer
  *
- * Description: singly linked list node structure
+ * Description: singly
  * for Holberton project
  */
 typedef struct listint_s
@@ -21,266 +21,22 @@ typedef struct listint_s
   struct listint_s *next;
 } listint_t;
 
-/**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- * for Holberton project
- */
-typedef struct skiplist_s
-{
-  int n;
-  size_t index;
-  struct skiplist_s *next;
-  struct skiplist_s *express;
-} skiplist_t;
+/*FUNCTION*/
 
-/* their prototypes */
+/*ARRAYS ALGORITHMS*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+
+/*LINKED LIST*/
+void free_list(listint_t *list);
+listint_t *create_list(int *array, size_t size);
+void print_list(const listint_t *list);
+
+/*LINKED ALGORITHMS*/
 listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
 
-/* mine */
-void print_it(int *arr, int begin, int end);
-int re_binary(int *array, int value, size_t low, size_t high);
-int do_binary(int *array, int value, size_t low, size_t high);
-
-#endif /* SEARCH_ALGOS_H */#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
-typedef struct listint_s
-{
-  int n;
-  size_t index;
-  struct listint_s *next;
-} listint_t;
-
-/**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- * for Holberton project
- */
-typedef struct skiplist_s
-{
-  int n;
-  size_t index;
-  struct skiplist_s *next;
-  struct skiplist_s *express;
-} skiplist_t;
-
-/* their prototypes */
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-int jump_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
-
-/* mine */
-void print_it(int *arr, int begin, int end);
-int re_binary(int *array, int value, size_t low, size_t high);
-int do_binary(int *array, int value, size_t low, size_t high);
-
-#endif /* SEARCH_ALGOS_H */#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
-typedef struct listint_s
-{
-  int n;
-  size_t index;
-  struct listint_s *next;
-} listint_t;
-
-/**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- * for Holberton project
- */
-typedef struct skiplist_s
-{
-  int n;
-  size_t index;
-  struct skiplist_s *next;
-  struct skiplist_s *express;
-} skiplist_t;
-
-/* their prototypes */
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-int jump_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
-
-/* mine */
-void print_it(int *arr, int begin, int end);
-int re_binary(int *array, int value, size_t low, size_t high);
-int do_binary(int *array, int value, size_t low, size_t high);
-
-#endif /* SEARCH_ALGOS_H */#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
-typedef struct listint_s
-{
-  int n;
-  size_t index;
-  struct listint_s *next;
-} listint_t;
-
-/**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- * for Holberton project
- */
-typedef struct skiplist_s
-{
-  int n;
-  size_t index;
-  struct skiplist_s *next;
-  struct skiplist_s *express;
-} skiplist_t;
-
-/* their prototypes */
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-int jump_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
-
-/* mine */
-void print_it(int *arr, int begin, int end);
-int re_binary(int *array, int value, size_t low, size_t high);
-int do_binary(int *array, int value, size_t low, size_t high);
-
-#endif /* SEARCH_ALGOS_H */#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
-typedef struct listint_s
-{
-  int n;
-  size_t index;
-  struct listint_s *next;
-} listint_t;
-
-/**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- * for Holberton project
- */
-typedef struct skiplist_s
-{
-  int n;
-  size_t index;
-  struct skiplist_s *next;
-  struct skiplist_s *express;
-} skiplist_t;
-
-/* their prototypes */
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-int jump_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
-
-/* mine */
-void print_it(int *arr, int begin, int end);
-int re_binary(int *array, int value, size_t low, size_t high);
-int do_binary(int *array, int value, size_t low, size_t high);
-
-#endif /* SEARCH_ALGOS_H */
+#endif /*SORT_ALGO*/
